89ed3802d2a7c18e361bf83f289879c836fbed82
MaudSocio <maud.yaiche@sciencespo.fr>
2026-02-20 15:52:05 +0100

    Premiere migration du profil et installation de la toolbar

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,14 +56,14 @@ ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
@@ -116,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Django Debug Toolbar
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 import datetime
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class Question(models.Model):
@@ -9,8 +11,17 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+)
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
 # On définit une classe, qu'on nomme choice, on a différent attribut de cette classe, question, voices.? 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # C'est lié à question
@@ -19,4 +30,20 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+    
+
+class Profil(models.Model):
+    class Genre(models.TextChoices):
+        Femme = 'F'
+        Homme = 'H'
+        Non_binaire = 'NB'
+        Autre = 'A'
+    name = models.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=(15)) 
+    biography = models.fields.CharField(max_length=1000)
+    birth_year = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
+    deja_ecrit_reve = models.fields.BooleanField(default=True)
+    page_officiel = models.fields.URLField(null=True, blank=True)
+    email = models.fields.EmailField(error_messages={"invalid": "Saisissez une adresse de courriel valide"})
+
 
