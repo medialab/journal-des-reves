706664c56ccd71a4f0670fb9b81df63bda5277b8
MaudSocio <maud.yaiche@sciencespo.fr>
2026-04-17 11:13:17 +0200

    Changement  pour dockerisation

--- settings.py
+++ settings.py
@@ -101,7 +101,6 @@ INSTALLED_APPS = [
 
 if DEBUG:
     INSTALLED_APPS.append('debug_toolbar')
-    INSTALLED_APPS.append('django_extensions')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
